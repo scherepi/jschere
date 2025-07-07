244cf75ec1ba5b346bfb0a02a492fa59f86a441e
scherepi <48649270+scherepi@users.noreply.github.com>
2025-07-07 12:42:27 -0500

    Put together skeleton for friends page

---
+++ friends/script.js
@@ -0,0 +1,14 @@
+// Copyright Joaquin Schere, begun 7/7/2025
+
+console.log("these are my amazing friends :)");
+
+// Get our nodes
+const gusDiv = document.getElementById("gus");
+const sebDiv = document.getElementById("sebastian");
+const megDiv = document.getElementById("meghana");
+const alexDiv = document.getElementById("alex");
+const sofDiv = document.getElementById("sofia");
+const louDiv = document.getElementById("lou");
+const gabDiv = document.getElementById("gabriel");
+
+// Animate Gus's div
