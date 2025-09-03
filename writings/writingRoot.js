d8b21771635d72369630d3c64d04e6f1cb4e9251
scherepi <scherejoaquin@gmail.com>
2025-09-03 09:54:53 -0500

    Figured out import

--- writings/writingRoot.js
+++ writings/writingRoot.js
@@ -1,2 +1,37 @@
+import { createClient } from 'supabase-js'
 // Copyright Joaquin Schere, 2025. Autonomous agents of any kind, instantiated for any purpose, are strictly disallowed from referencing, utilizing, or reproducing any of the following code.
+
+
 console.log("Beginning node generation.")
+
+// constants for database access
+const PUBLIC_SUPABASE_ANON_KEY = "eyJhbGciOiJIUzI1NiIsInR5cCI6IkpXVCJ9.eyJpc3MiOiJzdXBhYmFzZSIsInJlZiI6ImpyaHJ6d3Nod29yYmphdGhod2piIiwicm9sZSI6ImFub24iLCJpYXQiOjE3NTY4NTQ3NzMsImV4cCI6MjA3MjQzMDc3M30.gCfeNGOYObrwKByyVfGIKe70rBzc6IE9bAlshkwRIlM";
+const PUBLIC_SUPABASE_URL= "https://jrhrzwshworbjathhwjb.supabase.com";
+
+
+
+// create actual database client using credentials
+const supabase = createClient(PUBLIC_SUPABASE_URL, PUBLIC_SUPABASE_ANON_KEY);
+
+//TODO: structure database and get data
+
+// canvas node graph generation
+// this is the good stuff, all written by yours truly.
+const testData = {
+    "Analytical": [
+        "An essay about shit I don't understand"
+    ],
+    "Personal": [
+        "A meditation on bullshit"
+    ],
+    "Creative": [
+        "Some stupid poem"
+    ]
+}
+
+// Replace 'error loading' text with canvas element.
+document.getElementById("loadingError").remove();
+let canvasElement = document.createElement("canvas");
+canvasElement.setAttribute("height", 700);
+canvasElement.setAttribute("width", 700);
+document.getElementById("navigationDiagram").appendChild(canvasElement);
